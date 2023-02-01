391894b99a062d1298aaad0fb2920e22ff0892a2
Rafael Lopes <79181915+rafalopesdev@users.noreply.github.com>
2023-02-01 00:36:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.js-scroll');
+
+if (sections.length) {
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) section.classList.add('ativo');
+    });
+  }
+  animaScroll();
+
+  window.addEventListener('scroll', animaScroll);
+}
